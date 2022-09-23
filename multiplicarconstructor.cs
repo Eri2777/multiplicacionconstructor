8f15069b03f5226623eaacc952c1b83a5d1e6e91
Eri2777 <114264153+Eri2777@users.noreply.github.com>
2022-09-23 16:30:58 -0600

    Add files via upload

---
+++ multiplicarconstructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            multiplicar MULT = new multiplicar();
+            MULT.V1 = double.Parse(textBox1.Text);
+            MULT.V2 = double.Parse(textBox2.Text);
+
+            textBox3.Text = MULT.multipica().ToString();
+        }
+    }
+}
